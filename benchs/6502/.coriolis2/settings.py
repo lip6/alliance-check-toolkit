b468299ab4711c615e1de7055fa9249504acd4d0
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2018-03-19 14:18:28 +0100

    Update pop11 & 6502 for techno and routingGauge parameter.

--- benchs/6502/.coriolis2/settings.py
+++ benchs/6502/.coriolis2/settings.py
@@ -25,27 +25,18 @@ parametersTable = \
    # Kite parameters.
     , ("kite.eventsLimit"         , TypeInt       , 1000000 )
     , ('katabatic.topRoutingLayer', TypeString    , 'METAL5')
-    , ('anabatic.routingGauge'    , TypeString    ,  'sxlib')
    #, ("kite.hTracksReservedLocal", TypeInt       , 4       )
    #, ("kite.vTracksReservedLocal", TypeInt       , 4       )
     )
 
-if helpers.techno == '180/scn6m_deep_09':
-  parametersTable = helpers.overload \
-                    ( parametersTable
-                    , ( ('anabatic.routingGauge', TypeString, 'msxlib')
-                      ,
-                      )
-                    )
-
-  cellsTop = os.path.abspath( os.getcwd()+'/../cells' )
+cellsTop = os.path.abspath( os.getcwd()+'/../cells' )
   
-  allianceConfig = \
-      ( ( 'CLOCK'         , 'ck.*|m_clock')
-      , ( 'SYSTEM_LIBRARY', ( (cellsTop+'/nsxlib'  , Environment.Prepend)
-                            , (cellsTop+'/mpxlib'  , Environment.Prepend)) )
-      ,
-      )
+allianceConfig = \
+    ( ( 'CLOCK'         , 'ck.*|m_clock')
+    , ( 'SYSTEM_LIBRARY', ( (cellsTop+'/nsxlib'  , Environment.Prepend)
+                          , (cellsTop+'/mpxlib'  , Environment.Prepend)) )
+    ,
+    )
 
 #import os
 #
