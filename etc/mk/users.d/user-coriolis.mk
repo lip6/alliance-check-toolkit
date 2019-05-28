42a2d8b762f5b2c8fbb90fafcf40bbe12c8a91cb
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2019-05-28 13:46:48 +0200

    Setup nightly build parameters for Coriolis correctly.

--- etc/mk/users.d/user-coriolis.mk
+++ etc/mk/users.d/user-coriolis.mk
@@ -7,6 +7,7 @@
  DEBUG_OPTION   =
  BUILD_TYPE_DIR = Release.Shared
 
- CORIOLIS_TOP   = $(HOME)$(NIGHTLY)/coriolis-2.x/$(BUILD_VARIANT)$(LIB_SUFFIX_)/$(BUILD_TYPE_DIR)/install
- ALLIANCE_TOP   = /soc/alliance
- CHECK_TOOLKIT  = $(HOME)$(NIGHTLY)/coriolis-2.x/src/alliance-check-toolkit
+ export CORIOLIS_TOP   = $(HOME)$(NIGHTLY)/coriolis-2.x/$(BUILD_VARIANT)$(LIB_SUFFIX_)/$(BUILD_TYPE_DIR)/install
+ export ALLIANCE_TOP   = /soc/alliance
+ export CHECK_TOOLKIT  = $(HOME)$(NIGHTLY)/coriolis-2.x/src/alliance-check-toolkit
+ export YOSYS_TOP      = /usr
