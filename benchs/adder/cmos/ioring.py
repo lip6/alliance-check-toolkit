13214479c8f44153a3c6e4e9bcd3e43aff78d51c
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2019-08-08 01:00:48 +0200

    Update for the bidirectional & tristate support of CoreToChip.

---
+++ benchs/adder/cmos/ioring.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from helpers import l, u, n
+
+
+chip = { 'pads.ioPadGauge' : 'pxlib'
+       , 'pads.south'      : [ 'a_1', 'p_vddick_0', 'p_vssick_0' , 'a_0'       ]
+       , 'pads.east'       : [ 'a_2', 'a_3'       , 'b_3'        , 'b_2'       ]
+       , 'pads.north'      : [ 'b_1', 'p_vddeck_0', 'b_0'        , 'p_vsseck_0', 'p_reset' ]
+       , 'pads.west'       : [ 'f_3', 'f_2'       , 'p_m_clock_0', 'f_1'       , 'f_0'     ]
+       , 'core.size'       : ( l( 800), l( 800) )
+       , 'chip.size'       : ( l(2000), l(2000) )
+       , 'chip.clockTree'  : True
+       }
