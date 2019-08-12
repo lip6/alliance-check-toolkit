8d4e5384e209fde56585538563770ee8cf3b805a
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2019-08-13 00:23:13 +0200

    Shrink bottom of i1 terminal of ao22_x2.

---
+++ benchs/snx/phenitec06/.coriolis2/katana.py
@@ -0,0 +1,19 @@
+
+from Hurricane import DebugSession
+
+#DebugSession.addToTrace( katana.getCell().getNet( 'core.snx_inst.aon22_x1_219_sig' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'core.snx_inst.not_v_net_4' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'core.snx_inst.not_v_net_22' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'core.gr.not_v_net_93' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'core.snx_inst.oai22_x1_27_sig' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'core.snx_inst.oan21_x2_34_sig' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'adrs(7)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'inst(4)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'iadrs(15)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'm_clock' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'adrs(10)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'inst(8)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'datai(0)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'v_alu_b(15)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'core.snx_inst.n_exec' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'abc_5119_n791' ) )
