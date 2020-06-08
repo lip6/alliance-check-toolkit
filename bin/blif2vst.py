1d58a07a6852277235978edd9585a50886673d6b
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2020-06-08 13:54:33 +0200

    Add --vst-no-lowercase to blif2vst.py.

--- bin/blif2vst.py
+++ bin/blif2vst.py
@@ -50,19 +50,21 @@ def renameNMigenUniquify ( topCell ):
 if __name__ == '__main__':
 
     parser = optparse.OptionParser()
-    parser.add_option( '-c', '--cell'  , type='string',                      dest='cellName'     , help='The name of the BLIF to convert, without extension.')
-    parser.add_option( '-v', '--verbose'              , action='store_true', dest='verbose'      , help='First level of verbosity.')
-    parser.add_option( '-V', '--very-verbose'         , action='store_true', dest='veryVerbose'  , help='Second level of verbosity.')
-    parser.add_option(       '--vst-use-concat'       , action='store_true', dest='vstUseConcat' , help='The VST driver will use "&" (concat) in PORT MAP.')
+    parser.add_option( '-c', '--cell'            , type='string'      , dest='cellName'      , help='The name of the BLIF to convert, without extension.')
+    parser.add_option( '-v', '--verbose'         , action='store_true', dest='verbose'       , help='First level of verbosity.')
+    parser.add_option( '-V', '--very-verbose'    , action='store_true', dest='veryVerbose'   , help='Second level of verbosity.')
+    parser.add_option(       '--vst-use-concat'  , action='store_true', dest='vstUseConcat'  , help='The VST driver will use "&" (concat) in PORT MAP.')
+    parser.add_option(       '--vst-no-lowercase', action='store_true', dest='vstNoLowerCase', help='The VST will keep the case of all identifiers.')
     (options, args) = parser.parse_args()
     
-    views    = CRL.Catalog.State.Logical
+    views = CRL.Catalog.State.Logical
     if options.verbose:
         Cfg.getParamBool('misc.verboseLevel1').setBool(True)
     if options.veryVerbose:
         Cfg.getParamBool('misc.verboseLevel1').setBool(True)
         Cfg.getParamBool('misc.verboseLevel2').setBool(True)
-    if options.vstUseConcat: views    |= CRL.Catalog.State.VstUseConcat
+    if options.vstUseConcat:   views |= CRL.Catalog.State.VstUseConcat
+    if options.vstNoLowerCase: views |= CRL.Catalog.State.VstNoLowerCase
       
     cell = CRL.Blif.load( options.cellName )
     if cell.getName() == 'top':
