c09b0c13903d64132eb192929bb1bb7eb56dbae9
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2020-08-11 15:37:11 +0200

    Rename "ScriptMain()" into "scriptMain()", more Python compliant.

--- cells/nrflib/check/scaleCell.py
+++ cells/nrflib/check/scaleCell.py
@@ -289,7 +289,7 @@ def scaleCell ( editor, sourceCell ):
   return scaledCell
 
 
-def ScriptMain ( **kw ):
+def scriptMain ( **kw ):
   global framework
 
   helpers.staticInitialization( quiet=True )
@@ -359,7 +359,7 @@ if __name__ == '__main__':
 
   print framework.getEnvironment().getPrint()
 
-  success = ScriptMain( **kw )
+  success = scriptMain( **kw )
   shellSuccess = 0
   if not success: shellSuccess = 1
 
