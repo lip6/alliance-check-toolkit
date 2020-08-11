b73b52bd05b4c16340f3004a263bc576488cb454
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2020-08-11 15:35:02 +0200

    Parameters tweak.

--- benchs/analog/gmChamla/coriolis2/settings.py
+++ benchs/analog/gmChamla/coriolis2/settings.py
@@ -53,7 +53,7 @@ Cfg.getParamBool      ( 'misc.catchCore'              ).setBool      ( False    
 Cfg.getParamBool      ( 'misc.info'                   ).setBool      ( False     )
 Cfg.getParamBool      ( 'misc.paranoid'               ).setBool      ( False     )
 Cfg.getParamBool      ( 'misc.bug'                    ).setBool      ( False     )
-Cfg.getParamBool      ( 'misc.logMode'                ).setBool      ( True      )
+Cfg.getParamBool      ( 'misc.logMode'                ).setBool      ( False     )
 Cfg.getParamBool      ( 'misc.verboseLevel1'          ).setBool      ( True      )
 Cfg.getParamBool      ( 'misc.verboseLevel2'          ).setBool      ( True      )
 #Cfg.getParamInt       ( 'misc.minTraceLevel'          ).setInt       ( 159       )
