a2ae1689326b541c11bdbb36b3a8325bdd1fcacb
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2020-11-23 22:56:57 +0100

    Temporarily remove 6502/cmos45.

--- bin/go.sh
+++ bin/go.sh
@@ -24,7 +24,7 @@
  benchs="${benchs} AM2901/standart_cells/cmos"
  benchs="${benchs} AM2901/datapath/cmos"
  benchs="${benchs} 6502/cmos"
- benchs="${benchs} 6502/cmos45"
+#benchs="${benchs} 6502/cmos45"
 #benchs="${benchs} arlet6502/cmos350"
  benchs="${benchs} MIPS/microprogrammed"
  benchs="${benchs} MIPS/pipeline"
