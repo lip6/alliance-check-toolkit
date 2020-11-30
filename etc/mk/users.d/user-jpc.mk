d493f7d615db3dc7137b1dc66978e46a911438ad
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2020-11-30 17:46:39 +0100

    NDA_TOP selection according to network for jpc.

--- etc/mk/users.d/user-jpc.mk
+++ etc/mk/users.d/user-jpc.mk
@@ -1,6 +1,10 @@
 # Where Jean-Paul Chaput gets his tools installeds.
 
- export NDA_TOP       = ${HOME}/crypted/soc/techno
+ ifeq ($(shell hostname),lepka)
+     export NDA_TOP       = ${HOME}/crypted/soc/techno
+ else
+     export NDA_TOP       = /users/soft/techno/techno
+ endif
  export AMS_C35B4     = ${NDA_TOP}/AMS/035hv-4.10
  export FreePDK_45    = ${HOME}/coriolis-2.x/work/DKs/FreePDK45
  export FlexLib_CM018 = ${NDA_TOP}/etc/coriolis2/NDA/node180/tsmc_c018
