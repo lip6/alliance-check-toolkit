491093ba6b39d3d22a766d974d996d430de34deb
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2021-06-01 13:01:37 +0200

    In the cleaning step, prepend "cmpt_" to the model names.

--- etc/mk/synthesis-yosys.mk
+++ etc/mk/synthesis-yosys.mk
@@ -32,7 +32,8 @@
  NETLISTS_NOMODELS = $(foreach netlist,$(NETLISTS_NOCORONA),$(shell echo $(netlist) | sed 's:.*corona::'))
  VLOG_MODULE       = $(firstword $(NETLISTS_NOMODELS))
  NETLISTS_VST      = $(shell echo $(VLOG_MODULE) | tr '[:upper:]' '[:lower:]').vst 
- NETLISTS_SYNTH    = $(foreach netlist,$(NETLISTS_NOMODELS), $(shell echo $(netlist) | tr '[:upper:]' '[:lower:]'))
+ NETLISTS_LOWER    = $(foreach netlist,$(NETLISTS_NOMODELS), $(shell echo $(netlist) | tr '[:upper:]' '[:lower:]'))
+ NETLISTS_SYNTH    = $(foreach netlist,$(NETLISTS_LOWER), $(shell echo cmpt_$(netlist) $(netlist)))
  CLEAN_SYNTHESIS   = $(addsuffix .vst,$(NETLISTS_SYNTH)) \
                      $(addsuffix .il ,$(NETLISTS_SYNTH)) \
                      $(VLOG_MODULE).il $(VLOG_MODULE).blif $(VLOG_MODULE).tcl
