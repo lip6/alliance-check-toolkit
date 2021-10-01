652bac07133bbc1595cedfdfff2dafbf3a5da63a
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2021-10-01 12:54:55 +0200

    Add GDS versions of LIP6, SU, LibreSOC & C4M logos.

---
+++ benchs/PLL/tsmc_c018/loadGds.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os.path
+import Cfg
+import Hurricane
+from   Hurricane  import DbU, DataBase, UpdateSession, Library, Cell
+import Viewer     
+import CRL        
+from   helpers.io      import ErrorMessage
+from   helpers.overlay import UpdateSession
+import Unicorn
+
+
+def scriptMain ( **kw ):
+    editor = None
+    if kw.has_key('editor') and kw['editor']:
+      editor = kw['editor']
+  
+    with UpdateSession():
+        rootLib = DataBase.getDB().getRootLibrary()
+        gdsLib  = rootLib.getLibrary( 'GDS' )
+        if not gdsLib:
+            print 'Creating GDS library.'
+            gdsLib = Library.create( rootLib, 'GDS' )
+       #cell = Cell.create( gdsLib, 'quad_reference' )
+        CRL.Gds.load( gdsLib, './C4MLogo.gds' )
+        cell = gdsLib.getCell( 'gds_nazca' )
+        #cell = Cell.create( gdsLib, 'inv_x1' )
+        #CRL.Gds.load( gdsLib, './inv_x1.gds' )
+        if editor: editor.setCell( cell )
+  
+    return cell
