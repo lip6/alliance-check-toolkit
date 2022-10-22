263ab927550689f7b90357708fadb9cc5ed40e96
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2022-10-22 16:42:40 +0200

    Update snx/sxlib2M to be used a test bench for channel routing mode.

---
+++ benchs/snx/sxlib2M/coriolis2/katana.py
@@ -0,0 +1,10 @@
+
+from Hurricane import DebugSession
+
+#DebugSession.addToTrace( katana.getCell().getNet( 'cr3(14)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'cr3(8)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'subckt_636_inc16.abc_5134_new_n34' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'abc_5407_new_n599' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'subckt_634_alu16.cla_in2(12)' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'subckt_634_alu16.subckt_103_cla16.abc_5051_new_n104' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'subckt_634_alu16.subckt_103_cla16.abc_5051_new_n99' ) )
