c34adf568ac3b98670c1b92eaaaf3d481a5fd515
franck wajsburt <franck.wajsburt@lip6.fr>
2023-04-21 13:50:57 +0200

    New exp files to get sky130_lsx.rds and cmos.rds is renamed symbolic.rds

--- etc/mk/dks.d/lsxlib.mk
+++ etc/mk/dks.d/lsxlib.mk
@@ -1,9 +1,8 @@
 
 #export RDS_TECHNO_NAME = /soc/alliance/etc/cmos.rds
-#export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/cmos.rds
- export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/sky130d1_2.rds
- export SPI_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/C4M_LIP6_Sky130_hitas.spice
- export SPI_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/C4M_LIP6_Sky130.spice
+#export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/symbolic.rds
+ export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/sky130_lsx.rds
+ export SPI_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/C4M.Sky130_tt_model_hitas.spice
  export      CELLS_TOP  = $(CHECK_TOOLKIT)/cells
  export MBK_TARGET_LIB  = ${CELLS_TOP}/lsxlib
  export   MBK_CATA_LIB  = $(MBK_TARGET_LIB)
