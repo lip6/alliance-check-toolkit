d3c465971fa679cc40183541438173389c075960
franck wajsburt <franck.wajsburt@lip6.fr>
2023-04-21 07:27:23 +0200

    modfications to pass proof validation

--- etc/mk/dks.d/lsxlib.mk
+++ etc/mk/dks.d/lsxlib.mk
@@ -1,7 +1,9 @@
 
-#export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/sky130d1_1.rds
- export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/cmos.rds
-#export SPI_TECHNO_NAME = $(CHECK_TOOLKIT)/etc/scn6m_deep.hsp
+#export RDS_TECHNO_NAME = /soc/alliance/etc/cmos.rds
+#export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/cmos.rds
+ export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/sky130d1_2.rds
+ export SPI_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/C4M_LIP6_Sky130_hitas.spice
+ export SPI_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/C4M_LIP6_Sky130.spice
  export      CELLS_TOP  = $(CHECK_TOOLKIT)/cells
  export MBK_TARGET_LIB  = ${CELLS_TOP}/lsxlib
  export   MBK_CATA_LIB  = $(MBK_TARGET_LIB)
