2ce806a35db40eeaed15110bf7c41798243d356b
franck wajsburt <franck.wajsburt@lip6.fr>
2023-05-12 20:53:38 +0200

    sram cells + few modifications techno files

--- etc/mk/dks.d/lsxlib.mk
+++ etc/mk/dks.d/lsxlib.mk
@@ -1,7 +1,7 @@
 
 #export RDS_TECHNO_NAME = /soc/alliance/etc/cmos.rds
-#export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/symbolic.rds
- export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/sky130_lsx.rds
+export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/symbolic.rds
+# export RDS_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/sky130_lsx.rds
  export SPI_TECHNO_NAME = $(CHECK_TOOLKIT)/cells/lsxlib/techno/C4M.Sky130_tt_model_hitas.spice
                SPIMODEL = $(CHECK_TOOLKIT)/cells/lsxlib/techno/spimodel.cfg
              SPI_FORMAT = spice
