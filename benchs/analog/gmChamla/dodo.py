e7ac7bd1ca8947db6f00df8e2d92e8e76d0289c7
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2023-10-24 15:28:56 +0200

    Add dodo files to analog design (after Coriolis meson switch).

---
+++ benchs/analog/gmChamla/dodo.py
@@ -0,0 +1,32 @@
+
+import socket
+from   pathlib import Path
+
+#useTechno = 'MOSIS'
+useTechno = 'AMS350'
+
+if useTechno == 'AMS350':
+    hostname = socket.gethostname()
+    if hostname.startswith('lepka'):
+        ndaTop = Path( '/dsk/l1/jpc/crypted/soc/techno' )
+        if not ndaTop.is_dir():
+            print( '[ERROR] You forgot to mount the NDA encrypted directory, stupid!' )
+    else:
+        ndaTop = '/users/soft/techno/techno'
+    from coriolis.designflow.technos import setupAMS350
+    setupAMS350( checkToolkit='../../..', ndaTop=ndaTop )
+elif useTechno == 'MOSIS':
+    from coriolis.designflow.technos import setupMOSIS
+    setupMOSIS( checkToolkit='../../..' )
+
+DOIT_CONFIG = { 'verbosity' : 2 }
+
+from coriolis.designflow.pnr   import PnR
+from coriolis.designflow.clean import Clean
+PnR.textMode = True
+
+from gm  import scriptMain
+
+rulePnR   = PnR  .mkRule( 'pnr'  , [], [], scriptMain )
+ruleCgt   = PnR  .mkRule( 'cgt' )
+ruleClean = Clean.mkRule()
