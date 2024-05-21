adc47f8cbe121497fe4231956064e1d30d3efd64
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2024-05-21 12:02:14 +0200

    Added 8080 support for Sky130+NSxLib2

---
+++ benchs/8080/sky130_nsx2/dodo.py
@@ -0,0 +1,21 @@
+
+from coriolis.designflow.technos import setupSky130_nsx2
+
+setupSky130_nsx2( checkToolkit='../../..' )
+
+DOIT_CONFIG = { 'verbosity' : 2 }
+
+from coriolis.designflow.pnr      import PnR
+from coriolis.designflow.yosys    import Yosys
+from coriolis.designflow.blif2vst import Blif2Vst
+from coriolis.designflow.s2r      import S2R
+from coriolis.designflow.klayout  import DRC
+from coriolis.designflow.alias    import Alias
+from coriolis.designflow.clean    import Clean
+from coriolis.designflow          import pnrcheck
+PnR.textMode  = True
+
+from doDesign  import scriptMain
+
+pnrcheck.mkRuleSet( globals(), 'my80core', pnrcheck.UseClockTree )
+
