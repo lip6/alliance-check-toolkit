16c79aebb11d08f53c12c4af8b9e78f5c26aed88
Naohiko Shimizu <naohiko.shimizu@gmail.com>
2024-05-22 12:25:12 +0200

    modify dodo.py to use alternative DRC file for skywater 130

--- benchs/RISC-V/Minerva/sky130_nsx2/dodo.py
+++ benchs/RISC-V/Minerva/sky130_nsx2/dodo.py
@@ -1,7 +1,8 @@
+import pathlib
 
 from coriolis.designflow.technos import setupSky130_nsx2
-
-setupSky130_nsx2( checkToolkit='../../../..' )
+checkToolkit=pathlib.Path('../../../..')
+setupSky130_nsx2( checkToolkit )
 
 DOIT_CONFIG = { 'verbosity' : 2 }
 
@@ -16,6 +17,10 @@ from coriolis.designflow          import pnrcheck
 PnR.textMode  = True
 
 from doDesign  import scriptMain
+
+pdkDir          = checkToolkit / 'dks' / 'sky130_nsx2' / 'libs.tech'
+kdrcRules = pdkDir / 'klayout' / 'core' / 'sky130A_mr.drc'
+DRC.setDrcRules( kdrcRules )
 
 pnrcheck.mkRuleSet( globals(), 'minerva_cpu', pnrcheck.UseClockTree )
 
--- benchs/adder/sky130_nsx2/dodo.py
+++ benchs/adder/sky130_nsx2/dodo.py
@@ -1,7 +1,7 @@
-
+import pathlib
 from coriolis.designflow.technos import setupSky130_nsx2
-
-setupSky130_nsx2( checkToolkit='../../..' )
+checkToolkit=pathlib.Path('../../..')
+setupSky130_nsx2( checkToolkit)
 
 DOIT_CONFIG = { 'verbosity' : 2 }
 
@@ -16,6 +16,10 @@ from coriolis.designflow          import pnrcheck
 PnR.textMode  = True
 
 from doDesign  import scriptMain
+
+pdkDir          = checkToolkit / 'dks' / 'sky130_nsx2' / 'libs.tech'
+kdrcRules = pdkDir / 'klayout' / 'core' / 'sky130A_mr.drc'
+DRC.setDrcRules( kdrcRules )
 
 #pnrcheck.mkRuleSet( globals(), 'adder', pnrcheck.UseClockTree )
 pnrcheck.mkRuleSet( globals(), 'adder')
