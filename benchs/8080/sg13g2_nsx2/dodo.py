ab68565b64afdfe62bb11d03941c47c87c6dc191
Naohiko Shimizu <naohiko.shimizu@gmail.com>
2024-05-22 16:18:34 +0200

    add another IHP SG13G2 example for nsxlib2

---
+++ benchs/8080/sg13g2_nsx2/dodo.py
@@ -0,0 +1,39 @@
+import pathlib
+import sys
+from coriolis.designflow.technos import setupSky130_nsx2
+from coriolis.designflow.pnr      import PnR
+from coriolis.designflow.yosys    import Yosys
+from coriolis.designflow.blif2vst import Blif2Vst
+from coriolis.designflow.s2r      import S2R
+from coriolis.designflow.klayout  import DRC
+from coriolis.designflow.alias    import Alias
+from coriolis.designflow.clean    import Clean
+from coriolis.designflow          import pnrcheck
+from coriolis.designflow.task         import ShellEnv
+
+checkToolkit=pathlib.Path('../../..')
+pdkDir          = checkToolkit / 'dks' / 'sg13g2_nsx2' / 'libs.tech'
+coriolisTechDir = pdkDir / 'coriolis'
+sys.path.append( coriolisTechDir.as_posix() )
+from sg13g2_nsx2 import techno, nsxlib2, Sg13g2Setup 
+
+Sg13g2Setup.setupSg13g2_nsx2( checkToolkit )
+print("RDS=",ShellEnv.RDS_TECHNO_NAME)
+DOIT_CONFIG = { 'verbosity' : 2 }
+
+PnR.textMode  = True
+
+from doDesign  import scriptMain
+
+
+
+
+kdrcRules = pdkDir / 'klayout' /  'sg13g2.lydrc'
+DRC.setDrcRules( kdrcRules )
+
+pnrcheck.mkRuleSet( globals(), 'my80core', pnrcheck.UseClockTree )
+#pnrcheck.mkRuleSet( globals(), 'my80core')
+#ruleGds = S2R.mkRule( 'gds', 'my80core_cts_r.gds', 'my80core_cts_rdder_r.ap'
+#                    , flags=S2R.Verbose|S2R.NoReplaceBlackboxes )
+#ruleDRC = DRC.mkRule( 'drc', 'my80core_cts_r.gds' )
+
