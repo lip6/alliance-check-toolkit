f832a14099c0825ad573c9623da99a6eecf6b951
Naohiko Shimizu <naohiko.shimizu@gmail.com>
2024-05-27 14:12:34 +0200

    8080 Skywater: move technology setup to under the dks, add pinlayer to the gds

--- benchs/8080/sky130_nsx2/dodo.py
+++ benchs/8080/sky130_nsx2/dodo.py
@@ -1,3 +1,4 @@
+import sys
 import pathlib
 from coriolis.designflow.technos import setupSky130_nsx2
 checkToolkit=pathlib.Path('../../..')
@@ -15,11 +16,24 @@ from coriolis.designflow.clean    import Clean
 from coriolis.designflow          import pnrcheck
 PnR.textMode  = True
 
-from doDesign  import scriptMain
+#from doDesign  import scriptMain
+import doDesign
 
 pdkDir          = checkToolkit / 'dks' / 'sky130_nsx2' / 'libs.tech'
+coriolisTechDir = pdkDir / 'coriolis'
+sys.path.append( coriolisTechDir.as_posix() )
+from sky130_nsx2 import techno, nsxlib2, Sky130nsx2Setup 
+
 kdrcRules = pdkDir / 'klayout' / 'core' / 'sky130A_mr.drc'
+
+pdkCommonDir          = checkToolkit / 'dks' / 'common'  / 'coriolis'
+sys.path.append( pdkCommonDir.as_posix() )
+from s2r import S2R
+import pnrcheck
+
+S2R.flags = S2R.PinLayer | S2R.DeleteSubConnectors | S2R.Verbose|S2R.NoReplaceBlackboxes 
+
 DRC.setDrcRules( kdrcRules )
 
-pnrcheck.mkRuleSet( globals(), 'my80core', pnrcheck.UseClockTree )
+pnrcheck.mkRuleSet( globals(), doDesign.CoreName, pnrcheck.UseClockTree )
 
