0a58f4e883c5ef8212d67953230ba84e714f8bb4
Naohiko Shimizu <naohiko.shimizu@gmail.com>
2024-07-06 13:23:20 +0200

    remove absolute paths

--- benchs/inverter/skyWater130/sta/hitas/db_ng.tcl
+++ benchs/inverter/skyWater130/sta/hitas/db_ng.tcl
@@ -14,30 +14,30 @@ avt_config simVthHigh 0.8
 avt_config simVthLow 0.2
 avt_config simSlope 10e-12 
 
-avt_config simToolModel spice
+avt_config simToolModel hspice
 avt_config tasGenerateDetailTimingFile yes
 
 
 # Files of transistor model of the technology, that may require modifications
 #
 # nfet_01v8
-avt_LoadFile  /users/soft/freepdks/src/skywater-pdk/libraries/sky130_fd_pr/latest/cells/nfet_01v8/sky130_fd_pr__nfet_01v8__mismatch.corner.spice spice
-#avt_LoadFile  /users/soft/freepdks/src/skywater-pdk/libraries/sky130_fd_pr/latest/cells/nfet_01v8/sky130_fd_pr__nfet_01v8__tt.corner.spice spice
+avt_LoadFile  sky130_fd_pr__nfet_01v8__mismatch.corner.spice spice
+#avt_LoadFile  sky130_fd_pr__nfet_01v8__tt.corner.spice spice
 # modified to include the model file sky130_fd_pr__nfet_01v8__tt.pm3.spice with full path
 # modified sky130_fd_pr__nfet_01v8__tt.pm3.spice to use level = 14
 # have to use local files (hiererchical)
-avt_LoadFile  /users/cao/mariem/coriolis-2.x/src/alliance-check-toolkit/benchs/inverter/skyWater130/sta/techno/sky130_fd_pr__nfet_01v8__tt.corner.spice spice
+avt_LoadFile  techno/sky130_fd_pr__nfet_01v8__tt.corner.spice spice
 
 # pfet_01v8_hvt
-avt_LoadFile  /users/soft/freepdks/src/skywater-pdk/libraries/sky130_fd_pr/latest/cells/pfet_01v8_hvt/sky130_fd_pr__pfet_01v8_hvt__mismatch.corner.spice spice
-#avt_LoadFile  /users/soft/freepdks/src/skywater-pdk/libraries/sky130_fd_pr/latest/cells/pfet_01v8_hvt/sky130_fd_pr__pfet_01v8_hvt__tt.corner.spice spice
+avt_LoadFile  sky130_fd_pr__pfet_01v8_hvt__mismatch.corner.spice spice
+#avt_LoadFile  pfet_01v8_hvt/sky130_fd_pr__pfet_01v8_hvt__tt.corner.spice spice
 # modified to include the model file sky130_fd_pr__pfet_01v8_hvt__tt.pm3.spice with full path 
 #  modified sky130_fd_pr__pfet_01v8_hvt__tt.pm3.spice to use level = 14
 # have to use local files (hiererchical)
-avt_LoadFile  /users/cao/mariem/coriolis-2.x/src/alliance-check-toolkit/benchs/inverter/skyWater130/sta/techno/sky130_fd_pr__pfet_01v8_hvt__tt.corner.spice spice
+avt_LoadFile  sky130_fd_pr__pfet_01v8_hvt__tt.corner.spice spice
 
 # File decribing the netlist with power supply and temperature
-avt_LoadFile /users/cao/mariem/coriolis-2.x/src/alliance-check-toolkit/benchs/inverter/skyWater130/sta/inv_1_3_hitas.spi spice
+avt_LoadFile inv_1_3_hitas.spi spice
 
 # Database generation
 set fig [hitas sky130_fd_sc_hd__inv_1_3]
