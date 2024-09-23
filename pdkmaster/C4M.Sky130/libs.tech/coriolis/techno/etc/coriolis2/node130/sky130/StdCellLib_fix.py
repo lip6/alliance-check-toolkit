f99d14f92ad43bfd68a7c0ba6326ffd8f7d0a637
Staf Verhaegen <staf@fibraservi.eu>
2024-09-23 09:22:02 +0200

    Manual fix for SPICE netlist reading
    Signed-off-by: Staf Verhaegen <staf@fibraservi.eu>

--- pdkmaster/C4M.Sky130/libs.tech/coriolis/techno/etc/coriolis2/node130/sky130/StdCellLib_fix.py
+++ pdkmaster/C4M.Sky130/libs.tech/coriolis/techno/etc/coriolis2/node130/sky130/StdCellLib_fix.py
@@ -5,9 +5,5 @@ from coriolis.CRL import Spice
 
 def fix(lib):
     spiceDir = Path(__file__).parents[7] / 'libs.ref' / 'StdCellLib' / 'spice'
-    for spiceFile in os.listdir(spiceDir):
-        if spiceFile in ('StdCellLib.spi', "Gallery.spi):
-            continue
-        if not spiceFile.endswith('.spi'):
-            continue
-        Spice.load( lib, str(spiceDir / spiceFile), Spice.PIN_ORDERING )
+    Spice.load( lib, str(spiceDir / 'StdCellLib.spi'), Spice.PIN_ORDERING )
+
