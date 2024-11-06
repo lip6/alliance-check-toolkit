23c0f57a1a9e929f1368419cc56cca95d97fdb1e
Naohiko Shimizu <naohiko.shimizu@gmail.com>
2024-11-06 16:57:01 +0100

    IHP's OSDI model support

---
+++ benchs/inverter/sg130g2/env.sh
@@ -0,0 +1,2 @@
+export MBK_CATA_LIB=.:../../../dks/common/libs.tech/IHP-Open-PDK/ihp-sg13g2/libs.tech/ngspice/openvaf:../../../dks/common/libs.tech/IHP-Open-PDK/ihp-sg13g2/libs.tech/ngspice/models:../../../dks/sg13g2_nsx2/libs.tech/coriolis/sg13g2_nsx2
+
