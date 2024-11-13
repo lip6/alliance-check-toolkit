d29796b9188f6b2bc9df5c9fc1ff9cd33d5cbf2c
Naohiko Shimizu <naohiko.shimizu@gmail.com>
2024-11-13 13:18:13 +0100

    inverter for sg13g2

--- benchs/inverter/sg13g2/db_ng.tcl
+++ benchs/inverter/sg13g2/db_ng.tcl
@@ -13,11 +13,12 @@ avt_config avtVerboseConeFile yes
 avt_config simVthHigh 0.8
 avt_config simVthLow 0.2
 avt_config simSlope 10e-12 
+avt_config simTemperature 25.0
 
 avt_config simToolModel hspice
 avt_config tasGenerateDetailTimingFile yes
 
-avt_LoadFile ../openvaf/psp103_nqs.osdi osdi
+avt_LoadFile psp103_nqs.osdi osdi
 
 # Files of transistor model of the technology, that may require modifications
 #
