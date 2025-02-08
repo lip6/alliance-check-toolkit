c1bb9e95c53c2b5d53909a681724aaf7bbb65d14
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2025-02-08 12:44:52 +0100

    First STA integration in designflow for IHP SG13G2 on Arlet6502.

--- benchs/arlet6502/ihpsg13s2_c4m/dodo.py
+++ benchs/arlet6502/ihpsg13s2_c4m/dodo.py
@@ -13,7 +13,8 @@ from coriolis.designflow.yosys              import Yosys
 from coriolis.designflow.blif2vst           import Blif2Vst
 from coriolis.designflow.klayout            import Klayout
 from coriolis.designflow.pnr                import PnR
-from coriolis.designflow.sta                import STA, FMax
+from coriolis.designflow.lvx                import Lvx
+from coriolis.designflow.tasyagle           import TasYagle, STA, XTas
 from coriolis.designflow.alias              import Alias
 from coriolis.designflow.clean              import Clean
 from pdks.ihpsg13g2_c4m.designflow.filler   import Filler
@@ -21,10 +22,11 @@ from pdks.ihpsg13g2_c4m.designflow.sealring import SealRing
 from pdks.ihpsg13g2_c4m.designflow.drc      import DRC
 from doDesign                               import scriptMain
 
-buildChip    = False
-PnR.textMode = True
-pnrSuffix    = '_cts_r'
-topName      = 'arlet6502'
+buildChip          = False
+PnR.textMode       = True
+pnrSuffix          = '_cts_r'
+topName            = 'arlet6502'
+TasYagle.ClockName = 'clk'
 
 ruleYosys = Yosys   .mkRule( 'yosys', 'Arlet6502.v' )
 ruleB2V   = Blif2Vst.mkRule( 'b2v'  , 'arlet6502.vst', [ruleYosys], flags=0 )
@@ -47,17 +49,21 @@ if buildChip:
                                    , topName=topName )
 else:
     # Rule for block generation.
-    rulePnR   = PnR.mkRule( 'gds'  , [ 'Arlet6502_cts_r.gds'
+    rulePnR = PnR.mkRule( 'gds'    , [ 'Arlet6502_cts_r.gds'
                                      , 'arlet6502_cts_r.vst'
                                      , 'Arlet6502_cts_r.spi' ]
                                      , [ruleB2V]
                                    , scriptMain
                                    , topName=topName )
+    ruleLvx = Lvx.mkRule( 'lvx_spi', [ 'arlet6502_cts_r.vst'
+                                     , 'Arlet6502_cts_r.spi' ]
+                                   , Lvx.MergeSupply|Lvx.Flatten )
 
 ruleDrcMin  = DRC    .mkRule( 'drc_min', rulePnR.file_target(0), DRC.Minimal )
 ruleDrcMax  = DRC    .mkRule( 'drc_max', rulePnR.file_target(0), DRC.Maximal )
 ruleDrcC4M  = DRC    .mkRule( 'drc_c4m', rulePnR.file_target(0), DRC.C4M )
-ruleSTA     = FMax   .mkRule( 'fmax'   , 'yoyo', rulePnR.file_target(2))
+ruleSTA     = STA    .mkRule( 'sta'    , rulePnR.file_target(2))
+ruleXTas    = XTas   .mkRule( 'xtas'   , ruleSTA.file_target(0) )
 ruleCgt     = PnR    .mkRule( 'cgt' )
 ruleKlayout = Klayout.mkRule( 'klayout', depends=rulePnR.file_target(0) )
 ruleClean   = Clean  .mkRule( [ 'lefRWarning.log', 'cgt.log' ] )
