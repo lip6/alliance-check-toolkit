e548fc8036c02513f4b3b8c2b5bc901904817828
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2025-04-26 15:34:33 +0200

    Add support ISPD18, test 1, 2, 3 & 7.

---
+++ benchs/ISPD18/test7/coriolis2/katana.py
@@ -0,0 +1,7 @@
+
+from coriolis.Hurricane import DebugSession
+
+#DebugSession.addToTrace( katana.getCell().getNet( 'net67380' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'net79000' ) )
+DebugSession.addToTrace( katana.getCell().getNet( 'pin1' ) )
+
