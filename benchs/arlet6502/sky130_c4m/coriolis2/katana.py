b0e581c522513b8bdced943be84f94d81dd9fce9
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2025-04-30 13:18:35 +0200

    More signal that needed debug in arlet6502/sky130_c4m

--- benchs/arlet6502/sky130_c4m/coriolis2/katana.py
+++ benchs/arlet6502/sky130_c4m/coriolis2/katana.py
@@ -4,4 +4,6 @@ from coriolis.Hurricane import DebugSession
 #DebugSession.addToTrace( katana.getCell().getNet( 'cu_wr_rel_o(2)' ) )
 #DebugSession.addToTrace( katana.getCell().getNet( 'xer_so_ok' ) )
 #DebugSession.addToTrace( katana.getCell().getNet( 'abc_11867_new_n948' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'abc_11867_new_n2020' ) )
+#DebugSession.addToTrace( katana.getCell().getNet( 'abc_11867_new_n1848_hfns_0' ) )
 
