90b9b974f8339c6c8f925744484923d4db42471b
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2026-05-15 13:24:21 +0200

    Remove doit unordered DAG rules from log files (for deter check)

--- bin/gopy.sh
+++ bin/gopy.sh
@@ -23,6 +23,26 @@
           -e '/bytes$/d'            \
           -e '/[[:digit:]]s$/d'     \
           -e 's/([[:digit:]]+ sec)/(X sec)/' \
+          -e '/^(\.)|(--) +fifo32$/d'        \
+          -e '/^(\.)|(--) +fifo32_u01$/d'    \
+          -e '/^(\.)|(--) +fifo72$/d'        \
+          -e '/^(\.)|(--) +fifo129$/d'       \
+          -e '/^(\.)|(--) +shifter$/d'       \
+          -e '/^(\.)|(--) +decod_model$/d'   \
+          -e '/^(\.)|(--) +arm_core$/d'      \
+          -e '/^(\.)|(--) +exec$/d'          \
+          -e '/^(\.)|(--) +ifetch_model$/d'  \
+          -e '/^(\.)|(--) +alu$/d'           \
+          -e '/^(\.)|(--) +mem$/d'           \
+          -e '/^(\.)|(--) +reg$/d'           \
+          -e '/^(\.)|(--) +exec_model$/d'    \
+          -e '/^(\.)|(--) +ifetch$/d'        \
+          -e '/^(\.)|(--) +decod$/d'         \
+          -e '/^(\.)|(--) +muxs$/d'          \
+          -e '/^(\.)|(--) +ram$/d'           \
+          -e '/^(\.)|(--) +accu$/d'          \
+          -e '/^(\.)|(--) +muxe$/d'          \
+          -e '/^(\.)|(--) +coeur$/d'         \
           -e '/^End of script/d'    \
           -e '/^Time spent/d'       \
           -e 's/yosys-abc-[[:alnum:]]+/yosys-abc-XXXX/'  \
