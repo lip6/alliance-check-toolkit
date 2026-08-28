e718b44814e8180c131c9e2b89a7cbe5c2f6b77f
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2026-08-28 23:44:32 +0200

    Updating MIPS/pipeline config for latest Coriolis (fix errors).

--- benchs/MIPS/pipeline/coriolis2/katana.py
+++ benchs/MIPS/pipeline/coriolis2/katana.py
@@ -38,3 +38,17 @@ from coriolis.Hurricane import DebugSession
 #  Terminal horizontal segment Y 610.0L axis is outside RoutingPad [615.0L 635.0L].
 #  Segment constraints: [615.0L 635.0L]
 #DebugSession.addToTrace( katana.getCell().getNet( 'mips_r3000_1m_dp_instseqadr_sd_pi_0_12' ) )
+# [ERROR] AutoContactTerminal::getNativeConstraintBox(): Empty native constraints.
+#         On: <id:289705 ContactTerminal load_se VIA12 [2185.0L 1685.0L] 1.0L x 1.0L -T-----c->
+# --> dp_sff_x4_buf wen
+#DebugSession.addToTrace( katana.getCell().getNet( 'load_se' ) )
+# [ERROR] TrackSegment::getFreeInterval(): Segment not found in it's assigned track.
+#         * <VerticalTrack [18] METAL3 @90.0L [-3.5L:2953.5L] [1/1]> 
+#         * <id:390860 Vertical mips_r3000_1m_dp_banc_dinx(0) METAL3 [94.0L 10.0L] [94.0L 10.0L] 2.0L rpD:0 bl:0 F-----C---T-----------bb-- [9.0L:11.0L] [9.0L:11.0L] 2.0L -F----T--->
+#DebugSession.addToTrace( katana.getCell().getNet( 'mips_r3000_1m_dp_banc_dinx(0)' ) )
+# ERROR] TrackSegment::getFreeInterval(): Segment not found in it's assigned track. 
+#         * <VerticalTrack [242] METAL3 @1210.0L [-3.5L:2953.5L] [0/0]> 
+#         * <id:325919 Vertical mips_r3000_1m_dp_banc_nck METAL3 [1212.5L 1690.0L] [1212.5L 1690.0L] 2.0L rpD:0 bl:0 F-----C---T-----------bb-- [1689.0L:1691.0L] [1689.0L:1691.0L] 2.0L -F----T--->
+#DebugSession.addToTrace( katana.getCell().getNet( 'mips_r3000_1m_dp_banc_nck' ) )
+
+
